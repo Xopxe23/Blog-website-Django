4fa7d42d5f2fa8a02311af459447a9d831d4d409
Xopxe23 <georgeduday@gmail.com>
2023-02-07 12:40:53 +0300

    Создание связей между моделями

--- women/urls.py
+++ women/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
     path('post/<int:post_id>', show_post, name='post'),
+    path('category/<int:cat_id>', show_category, name='category'),
 ]
 
--- women/views.py
+++ women/views.py
@@ -11,10 +11,13 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
 
 def index(request):
     posts = Women.objects.all()
+    cats = Category.objects.all()
     context = {
         'menu': menu,
         'title': 'Главная страница',
-        'posts': posts
+        'posts': posts,
+        'cats': cats,
+        'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=context)
 
@@ -37,6 +40,21 @@ def about(request):
 
 def show_post(request, post_id):
     return HttpResponse(f'Отображение статьи с id = {post_id}')
+
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+    if len(posts) == 0:
+        raise Http404
+    context = {
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'posts': posts,
+        'cats': cats,
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=context)
 
 
 def pageNotFound(request, exception):
