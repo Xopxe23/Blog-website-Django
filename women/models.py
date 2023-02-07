a9523eb3a912b5959e84947f907e07abb3445d12
Xopxe23 <georgeduday@gmail.com>
2023-02-07 13:33:31 +0300

    Редактирование админ-панели

--- women/models.py
+++ women/models.py
@@ -3,13 +3,13 @@ from django.urls import reverse
 
 
 class Women(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d")
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    content = models.TextField(blank=True, verbose_name='Текст статьи')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d", verbose_name='Фото')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    is_published = models.BooleanField(default=True, verbose_name='Публикация')
 
     def __str__(self):
         return self.title
@@ -17,12 +17,22 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        verbose_name = 'Известная женщина'
+        verbose_name_plural = 'Известные женщины'
+        ordering = ['-time_create', 'title']
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Название категории')
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
