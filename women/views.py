0f617d337b3f472198482c2927e9d9d202183ade
Xopxe23 <georgeduday@gmail.com>
2023-02-07 23:08:53 +0300

    Добавление пользовательских тегов

--- women/views.py
+++ women/views.py
@@ -11,12 +11,10 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
 
 def index(request):
     posts = Women.objects.all()
-    cats = Category.objects.all()
     context = {
         'menu': menu,
         'title': 'Главная страница',
         'posts': posts,
-        'cats': cats,
         'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=context)
@@ -44,14 +42,12 @@ def show_post(request, post_id):
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
     if len(posts) == 0:
         raise Http404
     context = {
         'menu': menu,
         'title': 'Отображение по рубрикам',
         'posts': posts,
-        'cats': cats,
         'cat_selected': cat_id,
     }
     return render(request, 'women/index.html', context=context)
